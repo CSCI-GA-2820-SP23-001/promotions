ff25cd25cea27d695d30f307859fab529a01df71
yitao-zhou <yz4171@nyu.edu>
2023-03-21 20:00:57 +0000

    update postman collections

--- service/routes.py
+++ service/routes.py
@@ -19,7 +19,7 @@ from . import app
 def index():
     """ Root URL response """
     return (
-        "Reminder: return some useful information in json format about the service here",
+        "Status:OK",
         status.HTTP_200_OK,
     )
 
